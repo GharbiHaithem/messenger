4c5a4acd1573ea34b3585cf121832a2d83a35529
GharbiHaithem <135434595+GharbiHaithem@users.noreply.github.com>
2025-02-24 23:33:21 +0100

    Update uploadService.js

--- my-app/src/features/uploadService.js
+++ my-app/src/features/uploadService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const VITE_PUBLIC_URL   ="http://localhost:5001/api"
+const VITE_PUBLIC_URL   ="https://messenger-server-t9po.onrender.com/api"
 
 // import { config } from '../../utils/axiosConfig'
 
